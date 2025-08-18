20cca6a5ac3c9b7d4c11662e7c482e09c4051149
KH <khcheah@apexequity.com.my>
2025-08-18 10:42:17 +0800

    commit

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/send", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!"})
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World!!"})
 	})
 
 	r.Run(":8080")
