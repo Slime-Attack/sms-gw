a293dac5f9f53d2c5d2280a9333078fcf2dcbab2
Cheah KH <khcheah@apexequity.com.my>
2025-08-25 11:29:15 +0800

    Merge pull request #142 from Slime-Attack/hotfix/j2
    add log

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting Jimmy server...")
+	fmt.Println("Starting Jimmy 2nd try server...")
 
 	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
 
