e848c61d3d8c182924ab34b1e2de9a3679da621a
KH <khcheah@apexequity.com.my>
2025-08-25 12:08:48 +0800

    change resp

--- main.go
+++ main.go
@@ -49,7 +49,7 @@ func main() {
 
 	r.GET("/send", func(c *gin.Context) {
 		logrus.Infof("Hello, World!")
-		c.JSON(http.StatusOK, gin.H{"message": "Hello, World! Jimmy 4th try"})
+		c.JSON(http.StatusOK, gin.H{"message": "Hello, World! Jimmy 5th try"})
 	})
 
 	r.Run(":8080")
