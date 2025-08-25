4c3f77075a822f56257997b7816c7729b1c5eacd
KH <khcheah@apexequity.com.my>
2025-08-25 11:58:43 +0800

    change log

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting Jimmy 2nd try server...")
+	fmt.Println("Starting Jimmy 3rd try server...")
 
 	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
 
